3b93613faf91b3bb31b89bceafd6ed1d8fd5fdea
Amir Rezvani <106575880+AREZVANI95@users.noreply.github.com>
2022-07-16 16:23:42 -0400

    Update script.js
    Added a variable for the text box ID.
    Added two functions one to display the text when saved and the other to save whatever text you add in it.
    Added a event to run the saveText function.

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ var currentDay = moment();
 $('#currentDay').text(currentDay);
 // Variable for the button.
 var saveButton = document.getElementById("savBtn")
+// Variable for the Text Description
+var textBox = document.getElementById("descriptionID")
+
+function displayText() {
+
+};
+
+function saveText () {
+
+    var text = localStorage.getItem("descriptionID");
+}
+
+saveButton.addEventListener("click", saveText);
